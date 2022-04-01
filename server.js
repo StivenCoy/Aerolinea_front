59c1538424a10e59f3c152d4808238d31e48a7f2
EstibenCoy <stivenrodriguez1270@gmail.com>
2022-04-01 16:32:33 -0500

    dist

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/Aerolinea'));
+app.use(express.static('./dist/aerolinea'));
 
 app.get('/*', (req, res) => 
-    res.sendFile('index.html', { root: 'dist/Aerolinea/' })
+    res.sendFile('index.html', { root: 'dist/aerolinea/' })
 );
 
 app.listen(process.env.PORT || 8080);
